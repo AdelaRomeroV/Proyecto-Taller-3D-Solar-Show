7c61a680ec090498a3535cb4b4eff329f0a48687
Francisco <francisco.anticona81@gmail.com>
2024-05-15 15:10:13 -0500

    Aplastadores
    MMM... creo que esta bien

--- Assets/Aplastador.cs
+++ Assets/Aplastador.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class Aplastador : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform startPoint;
+    public float moveSpeed;
+    private Rigidbody rb;
+    public bool isFalling = true;
+    private Mov player;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Mov>();
+        StartCoroutine(CrushRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator CrushRoutine()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(2f);
+
+            isFalling = true;
+            while (isFalling)
+            {
+                DowCrusher();
+                yield return null;
+            }
+
+            while (Vector3.Distance(transform.position, startPoint.position) >= 0.1f)
+            {
+                UpCrushed();
+                yield return null;
+            }
+
+            transform.position = startPoint.position;
+        }
+    }
+
+    private void DowCrusher()
+    {
+        rb.MovePosition(transform.position + Vector3.down * 240 * Time.deltaTime);
+    }
+
+    private void UpCrushed()
+    {
+        rb.MovePosition(transform.position + Vector3.up * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            isFalling = false;
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isFalling = false;
+            player.velocidadActual = 0;
+            player.onStun = true;
+            player.Invoke("OffStun", 1f);
+        }
     }
 }
