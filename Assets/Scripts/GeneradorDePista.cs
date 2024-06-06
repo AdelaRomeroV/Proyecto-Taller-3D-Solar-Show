6f0d121aad7918cb287b488ff97f9b3000856e2a
AndreiMore <andreiloayza14@gmail.com>
2024-06-06 02:18:48 -0500

    [PW_17] Ahora se pueden crear desviaciones a los lados proceduralmente
    Tambien se agregaron Sets de Hazards para el Spawner de Hazards

--- Assets/Scripts/GeneradorDePista.cs
+++ Assets/Scripts/GeneradorDePista.cs
@@ -1,13 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GeneradorDePista : MonoBehaviour
 {
     public Transform Jugador;
 
-    public void GenerarPista(GameObject Pista)
+    public virtual void GenerarPista(GameObject Pista)
     {
         if (Vector3.Distance(Jugador.position, transform.position) <= 300)
         {
