e2788db7bd83535d4c001b3fef2115a216118d18
Francisco <francisco.anticona81@gmail.com>
2024-06-13 10:28:23 -0500

    Guardando escena y ordenando script

---
+++ Assets/Scripts/DynamicMov.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DynamicMov : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    private Transform currentPoint;
+
+    private void Start()
+    {
+        currentPoint = pointB;
+    }
+
+    void Update()
+    {
+        MovBumpers();
+    }
+
+    private void MovBumpers()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentPoint.position, 10 * Time.deltaTime);
+        if (Vector3.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+        if (Vector3.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
+    }
+}
