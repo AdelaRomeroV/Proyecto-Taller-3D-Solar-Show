a4d08f4554a9732d97fa69dda319b0f9c406b4b3
Francisco <francisco.anticona81@gmail.com>
2024-06-17 17:52:31 -0500

    Guardadno Comentario
    Tanka comento que cosas raras podria pasar, asi recomendo usar corrutina

--- Assets/Scripts/DynamicMov.cs
+++ Assets/Scripts/DynamicMov.cs
@@ -22,7 +22,7 @@ public class DynamicMov : MonoBehaviour
     private void MovBumpers()
     {
         transform.position = Vector3.MoveTowards(transform.position, currentPoint.position, 10 * Time.deltaTime);
-        if (Vector3.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        if (Vector3.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform) // convertir corrutina
         {
             currentPoint = pointA.transform;
         }
