8fc535375c76cc039c08b5f19af3b3eda23777e9
Francisco <francisco.anticona81@gmail.com>
2024-06-24 17:31:04 -0500

    Nivel 1 Añadiendo el cronometro inicial

--- Assets/Scripts/SonidoExplosion.cs
+++ Assets/Scripts/SonidoExplosion.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class SonidoExplosion : MonoBehaviour
 { 
-    [SerializeField]private AudioSource audio;
+    [SerializeField]private AudioSource audioSource;
 
     public void Sonido()
     {
-        audio.Play();
+        audioSource.Play();
     }
 }
