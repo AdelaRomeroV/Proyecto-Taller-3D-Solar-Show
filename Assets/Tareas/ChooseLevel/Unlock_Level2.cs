0a1c899f08f5f67b8dfa49104ba01a962a51aa8d
fabianaPerez24 <fabianalertora003@gmail.com>
2024-06-30 13:23:06 -0500

    update level select

--- Assets/Tareas/ChooseLevel/Unlock_Level2.cs
+++ Assets/Tareas/ChooseLevel/Unlock_Level2.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Unlock_Level2 : BottonBlock
 {
+    [SerializeField] Sprite miniatura;
     public GameManager gameManager;
-
+    Image Image;
     // Start is called before the first frame update
     void Awake()
     {
+        Image = GetComponent<Image>();
         gameManager = FindObjectOfType<GameManager>();
         button.onClick.AddListener(ChooseLevel2);
     }
@@ -17,8 +20,9 @@ public class Unlock_Level2 : BottonBlock
     void ChooseLevel2()
     {
         
-        if (gameManager.levels ==2)
+        if (gameManager.levels <=2)
         {
+            miniatura=Image.sprite;
             SceneManager.LoadScene("Level_02");
         }
     }
