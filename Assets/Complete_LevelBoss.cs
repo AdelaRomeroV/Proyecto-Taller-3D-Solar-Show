29e636431e92ffcbd49ff2728ad632dfa50b815a
AndreiMore <andreiloayza14@gmail.com>
2024-07-01 17:19:11 -0500

    Arreglo Nivel 2

---
+++ Assets/Complete_LevelBoss.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Complete_LevelBoss : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
